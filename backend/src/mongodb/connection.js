0d09c778fc2f70d7e08ed4d3d9726d6f7b3292f6
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2025-10-03 21:52:38 +0530

    env secured

--- backend/src/mongodb/connection.js
+++ backend/src/mongodb/connection.js
@@ -1,11 +1,11 @@
 // src/db.js
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
 
 (async () => {
     try {
-        await mongoose.connect('mongodb+srv://quadrant593:mubULtnialEeSsxy@quadrant.tewouce.mongodb.net/ieee', {
-            useNewUrlParser: true,
-        });
+        await mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,});
         console.log("✅ MongoDB Connected");
     } catch (err) {
         console.error("❌ MongoDB connection failed:", err.message);
